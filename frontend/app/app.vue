45b826667f1deb2f3b8b78d630ec3c281b495ada
chrinore <chrinoren@gmail.com>
2026-02-18 02:41:32 +0100

    first commit

---
+++ frontend/app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <AppLogo />
+    <Down />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// import AppLogo from "./components/AppLogo.vue";
+</script>
+
+<style></style>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+
+  nitro: {
+    output: { dir: "dist" },
+  },
+  modules: ["@nuxt/eslint", "@nuxt/ui"],
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ["~/assets/css/main.css"],
+
+  routeRules: {
+    "/": { prerender: true },
+  },
+
+  compatibilityDate: "2025-01-15",
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
